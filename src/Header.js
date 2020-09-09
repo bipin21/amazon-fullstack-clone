08fbe4bdc7864ba32ddceab5eb80a0d766780ee3
Bipin Karki <bipinkarki172@gmail.com>
2020-09-09 13:48:01 +0545

    Second commit

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Header.css';
+
+function Header() {
+    return (
+        <div className="header">
+             <img className="header__logo" src="amazon.png" />
+        </div>
+    )
+}
+
+export default Header
